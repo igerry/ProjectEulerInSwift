6c1b540b344b0442e589ea268897b1914ac101ef
Gerry Wang <Gerry.Wang@intelematics.com>
2016-08-18 15:13:09 +1000

    NEW - update result for question 3.

--- Euler/Problem_3.playground/Contents.swift
+++ Euler/Problem_3.playground/Contents.swift
@@ -7,6 +7,8 @@ let projectEuler = "projecteuler.net"
 /*
  Largest prime factor
  Problem 3
+ 6857
+ 
  The prime factors of 13195 are 5, 7, 13 and 29.
  
  What is the largest prime factor of the number 600851475143 
