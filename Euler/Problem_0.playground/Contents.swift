a7bbc9de390437743d879a17b57b27894e22e30f
Gerry Wang <Gerry.Wang@intelematics.com>
2016-08-18 13:54:12 +1000

    NEW - add problem 1,2.

---
+++ Euler/Problem_0.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let projectEuler = "projecteuler.net"
+
+/*
+ <title>
+ Problem <number>
+ <result>
+ 
+ <rescription>
+ 
+ */
+func problem0() -> Int {
+    let sum = 0
+    return sum
+}
+let p0 = problem0()
