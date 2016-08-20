38f69c38973923452b300587a9faeab11abfb2c6
Gerry Wang <igerry@gmail.com>
2016-08-20 19:06:17 +1000

    NEW - add problem 17.

--- ProjectEulerInSwift/main.swift
+++ ProjectEulerInSwift/main.swift
@@ -11,7 +11,7 @@ import Foundation
 let projectEuler = "projecteuler.net"
 let start = NSDate()
 
-print("[Result]: \(p16)")
+print("[Result]: \(p17)")
 
 let diff = -start.timeIntervalSinceNow
 print("[TotalTime]: \(diff)s")
